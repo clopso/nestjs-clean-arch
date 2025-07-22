4cb0fb4aad03f431a5febcaa31eb33a138062dbd
Caio Lemes Gomes dos Santos <caiolemes306@gmail.com>
2025-07-22 15:41:03 -0300

    ClassValidatorFields - unit tests - part 2

--- src/shared/domain/entities/validator/__tests__/unit/class-validator-fields.spec.ts
+++ src/shared/domain/entities/validator/__tests__/unit/class-validator-fields.spec.ts
@@ -27,4 +27,16 @@ describe("ClassValidatorFields unit tests", () => {
     expect(sut.validatedData).toBeUndefined()
     expect(sut.errors).toStrictEqual({ field: ["test error"]})
   })
+
+    it("Should validade without erros", () => {
+    const spyValidateSync = jest.spyOn(libClassValidator, "validateSync")
+    spyValidateSync.mockReturnValue([])
+
+    const sut = new StubClassValidatorFields()
+
+    expect(sut.validate({field: "value"})).toBeTruthy()
+    expect(spyValidateSync).toHaveBeenCalled()
+    expect(sut.validatedData).toStrictEqual({field: "value"})
+    expect(sut.errors).toBeUndefined()
+  })
 })
