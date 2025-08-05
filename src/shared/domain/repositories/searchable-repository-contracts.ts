1ed641faab6ad0cbf21e0d87a89e5dc2137ca978
Caio Lemes Gomes dos Santos <caiolemes306@gmail.com>
2025-08-05 15:51:42 -0300

    Adding SearchResult class

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -11,6 +11,16 @@ export type SearchProps<Filter = string> = {
   filter?: Filter | null;
 };
 
+export type SearchResultProps<E extends Entity, Filter> = {
+  items: E[];
+  total: number;
+  currentPage: number;
+  perPage: number;
+  sort: string | null;
+  sortDir: string | null;
+  filter: Filter | null;
+};
+
 export class SearchParams {
   protected _page: number;
   protected _perPage = 15;
@@ -30,50 +40,91 @@ export class SearchParams {
     return this._page;
   }
   private set page(value: number) {
-    let _page = value
+    let _page = value;
     if (Number.isNaN(_page) || _page <= 0 || parseInt(_page as any) !== _page) {
-      _page = 1
+      _page = 1;
     }
 
-    this._page = _page
+    this._page = _page;
   }
 
   get perPage() {
-    return this._perPage
+    return this._perPage;
   }
   private set perPage(value: number) {
-    let _perPage = value === (true as any) ? this._perPage : +value
-    if (Number.isNaN(_perPage) || _perPage <= 0 || parseInt(_perPage as any) !== _perPage) {
-      _perPage = this._perPage
+    let _perPage = value === (true as any) ? this._perPage : +value;
+    if (
+      Number.isNaN(_perPage) ||
+      _perPage <= 0 ||
+      parseInt(_perPage as any) !== _perPage
+    ) {
+      _perPage = this._perPage;
     }
 
-    this._perPage = _perPage
+    this._perPage = _perPage;
   }
 
   get sort() {
-    return this._sort
+    return this._sort;
   }
   private set sort(value: string | null) {
-    this._sort = value === null || value === undefined || value === '' ? null : `${value}`
+    this._sort =
+      value === null || value === undefined || value === '' ? null : `${value}`;
   }
 
   get sortDir() {
-    return this._sortDir
+    return this._sortDir;
   }
   private set sortDir(value: SortDirection | null) {
-    if(!this._sort) {
-      this._sortDir = null
-      return
+    if (!this._sort) {
+      this._sortDir = null;
+      return;
     }
-    const dir = `${value}`.toLowerCase()
-    this._sortDir = dir !== 'asc' && dir !== 'desc' ? 'desc' : dir
+    const dir = `${value}`.toLowerCase();
+    this._sortDir = dir !== 'asc' && dir !== 'desc' ? 'desc' : dir;
   }
 
   get filter() {
-    return this._filter
+    return this._filter;
   }
   private set filter(value: string | null) {
-    this._filter = value === null || value === undefined || value === '' ? null : `${value}`
+    this._filter =
+      value === null || value === undefined || value === '' ? null : `${value}`;
+  }
+}
+
+export class SearchResult<E extends Entity, Filter = string> {
+  readonly items: E[];
+  readonly total: number;
+  readonly currentPage: number;
+  readonly perPage: number;
+  readonly lastPage: number;
+  readonly sort: string | null;
+  readonly sortDir: string | null;
+  readonly filter: Filter | null;
+
+  constructor(props: SearchResultProps<E, Filter>) {
+    this.items = props.items
+    this.total = props.total
+    this.currentPage = props.currentPage
+    this.perPage = props.perPage
+    this.lastPage = Math.ceil(this.total / this.perPage)
+    this.sort = props.sort ?? null
+    this.sortDir = props.sortDir ?? null
+    this.filter = props.filter ?? null
+  }
+
+  toJSON(forceEntity = false) {
+    return {
+      items: forceEntity ? this.items.map(item => item.toJSON) : this.items,
+      total: this.total,
+      currentPage: this.currentPage,
+      perPage: this.perPage,
+      lastPage: this.lastPage,
+      sort: this.sort,
+      sortDir: this.sortDir,
+      filter: this.filter
+    }
   }
 }
 
