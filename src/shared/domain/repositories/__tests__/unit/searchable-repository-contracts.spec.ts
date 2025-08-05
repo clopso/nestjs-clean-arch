20b43c0a6e5420ff4010de0c4a739108b8c34d0f
Caio Lemes Gomes dos Santos <caiolemes306@gmail.com>
2025-08-05 16:31:49 -0300

    unit tests SearchResult class

--- src/shared/domain/repositories/__tests__/unit/searchable-repository-contracts.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/searchable-repository-contracts.spec.ts
@@ -1,5 +1,6 @@
 import {
   SearchParams,
+  SearchResult,
   SortDirection,
 } from '../../searchable-repository-contracts';
 
@@ -115,6 +116,76 @@ describe('Searchable Repository unit tests', () => {
           }).sortDir,
         ).toBe(param.expect);
       });
+    });
+  });
+
+  describe('SearchResult tests', () => {
+    it('constructor props', () => {
+      let sut = new SearchResult({
+        items: ['test1', 'test2', 'test3', 'test4'] as any,
+        total: 4,
+        currentPage: 1,
+        perPage: 2,
+        sort: null,
+        sortDir: null,
+        filter: null,
+      })
+
+      expect(sut.toJSON()).toStrictEqual({
+        items: ['test1', 'test2', 'test3', 'test4'] as any,
+        total: 4,
+        currentPage: 1,
+        perPage: 2,
+        lastPage: 2,
+        sort: null,
+        sortDir: null,
+        filter: null,
+      })
+
+      sut = new SearchResult({
+        items: ['test1', 'test2', 'test3', 'test4'] as any,
+        total: 4,
+        currentPage: 1,
+        perPage: 2,
+        sort: "name",
+        sortDir: "asc",
+        filter: "null" as any
+      })
+
+      expect(sut.toJSON()).toStrictEqual({
+        items: ['test1', 'test2', 'test3', 'test4'] as any,
+        total: 4,
+        currentPage: 1,
+        perPage: 2,
+        lastPage: 2,
+        sort: "name",
+        sortDir: "asc",
+        filter: "null" as any
+      })
+
+      sut = new SearchResult({
+        items: ['test1', 'test2', 'test3', 'test4'] as any,
+        total: 4,
+        currentPage: 1,
+        perPage: 10,
+        sort: "name",
+        sortDir: "asc",
+        filter: "null" as any
+      })
+
+      expect(sut.lastPage).toBe(1)
+
+      sut = new SearchResult({
+        items: ['test1', 'test2', 'test3', 'test4'] as any,
+        total: 54,
+        currentPage: 1,
+        perPage: 10,
+        sort: "name",
+        sortDir: "asc",
+        filter: "null" as any
+      })
+
+      expect(sut.lastPage).toBe(6)
     });
   });
 });
