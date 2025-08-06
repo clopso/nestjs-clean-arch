60e6b76a9d6f1ab367e6905561d28689ca2d4d6c
Caio Lemes Gomes dos Santos <caiolemes306@gmail.com>
2025-08-06 15:45:04 -0300

    Adding applySort method in InMemorySearchableRepository class

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -104,27 +104,27 @@ export class SearchResult<E extends Entity, Filter = string> {
   readonly filter: Filter | null;
 
   constructor(props: SearchResultProps<E, Filter>) {
-    this.items = props.items
-    this.total = props.total
-    this.currentPage = props.currentPage
-    this.perPage = props.perPage
-    this.lastPage = Math.ceil(this.total / this.perPage)
-    this.sort = props.sort ?? null
-    this.sortDir = props.sortDir ?? null
-    this.filter = props.filter ?? null
+    this.items = props.items;
+    this.total = props.total;
+    this.currentPage = props.currentPage;
+    this.perPage = props.perPage;
+    this.lastPage = Math.ceil(this.total / this.perPage);
+    this.sort = props.sort ?? null;
+    this.sortDir = props.sortDir ?? null;
+    this.filter = props.filter ?? null;
   }
 
   toJSON(forceEntity = false) {
     return {
-      items: forceEntity ? this.items.map(item => item.toJSON) : this.items,
+      items: forceEntity ? this.items.map((item) => item.toJSON) : this.items,
       total: this.total,
       currentPage: this.currentPage,
       perPage: this.perPage,
       lastPage: this.lastPage,
       sort: this.sort,
       sortDir: this.sortDir,
-      filter: this.filter
-    }
+      filter: this.filter,
+    };
   }
 }
 
@@ -134,5 +134,6 @@ export interface SearchableRepositoryInterface<
   SearchInput = SearchParams,
   SearchOutput = SearchResult<E, Filter>,
 > extends RepositoryInterface<E> {
+  sortableFields: string[];
   search(props: SearchParams): Promise<SearchOutput>;
 }
